6e4c0385082cf290086fa3905821ef92c2b60ee1
Niels <info@heimseiten.de>
2023-09-27 17:12:51 +0200

    Add files via upload

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,17 +14,24 @@ declare(strict_types=1);
 
 namespace Heimseiten\ContaoLesbaresCssFrameworkBundle\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Heimseiten\ContaoLesbaresCssFrameworkBundle\HeimseitenContaoLesbaresCssFrameworkBundle;
 
 class Plugin implements BundlePluginInterface
 {
-    public function getBundles(ParserInterface $parser): array
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create('Heimseiten\ContaoLesbaresCssFrameworkBundle\HeimseitenContaoLesbaresCssFrameworkBundle')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+            BundleConfig::create(HeimseitenContaoLesbaresCssFrameworkBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                ]),
         ];
     }
 }
