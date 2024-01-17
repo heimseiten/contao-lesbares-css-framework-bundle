018068847a6aeb4ac531b74c49f41e08e9c360d1
Niels <info@heimseiten.de>
2024-01-17 12:13:49 +0100

    Add files via upload

---
+++ contao/dca/tl_settings.php
@@ -0,0 +1,16 @@
+<?php
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\DataContainer;
+
+PaletteManipulator::create()
+    ->addLegend('lesbaresCSSFramework', 'backend_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('doNotUseCssFrameworkCssFile', 'lesbaresCSSFramework', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_settings') 
+;
+
+$GLOBALS['TL_DCA']['tl_settings']['fields']['doNotUseCssFrameworkCssFile'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_settings']['doNotUseCssFrameworkCssFile'],
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w25'),
+    'sql'       => "char(1) NOT NULL default ''" 
+];
