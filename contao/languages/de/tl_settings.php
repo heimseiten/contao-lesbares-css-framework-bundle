751e8e550b1156b0c6f1ac866f68f1141ef0e0c7
Niels <info@heimseiten.de>
2024-01-17 12:20:20 +0100

    Add files via upload

---
+++ contao/languages/de/tl_settings.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// Legends
+$GLOBALS['TL_LANG']['tl_settings']['lesbaresCSSFramework'] = 'lesbares CSS Framework';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_settings']['doNotUseCssFrameworkCssFile'] = ['CSS Datei vom lesbaren CSS Framework nicht verwenden', ''];
