150caa5aecc9c13b199abb51d8cd3fb1ac3dedd3
Niels <info@heimseiten.de>
2024-03-28 15:08:15 +0100

    Delete contao/languages/de directory

--- contao/languages/de/tl_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// Legends
-$GLOBALS['TL_LANG']['tl_settings']['lesbaresCSSFramework'] = 'lesbares CSS Framework';
-
-// Fields
-$GLOBALS['TL_LANG']['tl_settings']['doNotUseCssFrameworkCssFile'] = ['CSS Datei vom lesbaren CSS Framework nicht verwenden', ''];
