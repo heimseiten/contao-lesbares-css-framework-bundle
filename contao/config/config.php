77e672558a037cb67d51ed008cb4454e23625e72
Niels <info@heimseiten.de>
2024-10-10 17:08:24 +0200

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,6 +3,5 @@
 use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
-if ( !Contao\Config::get('doNotUseCssFrameworkCssFile') ) {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-framework-bundle.scss|static';
-}
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-framework-bundle.scss|static';
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-grid-framework.scss|static';
