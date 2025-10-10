2a454a23b857a687527ba8dcba17a71f3cad00a6
Niels <info@heimseiten.de>
2025-10-10 11:39:08 +0200

    Update CSS file references in config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -3,5 +3,5 @@
 use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
-$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-framework-bundle.scss|static';
-$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-grid-framework.scss|static';
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-framework-bundle.css|static';
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaolesbarescssframework/contao-lesbares-css-grid-framework.css|static';
